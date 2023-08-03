533960e208dcd3db26f86801147a07ba60991ce1
Tolu <tolugbesan@gmail.com>
2023-08-03 03:39:23 +0100

    Add functionality to generate humanity proof

---
+++ proof.js
@@ -0,0 +1,29 @@
+import { ethers } from "ethers";
+import 'dotenv/config'
+
+const validatorKey = process.env.VALIDATOR_PRIVATE_KEY
+const wallet = new ethers.Wallet(validatorKey);
+
+async function generateProof(data, timestamp) {
+    const proofTimestamp = ethers.utils.hexZeroPad(
+      ethers.utils.hexlify(Math.floor(timestamp.getTime() / 1000)),
+      4
+    );
+
+    const hash = ethers.utils.keccak256(
+      ethers.utils.hexConcat([data, proofTimestamp])
+    );
+    const validatorSignature = await this.wallet.signMessage(
+      ethers.utils.arrayify(hash)
+    );
+
+    const proof = ethers.utils.hexConcat([
+      data,
+      proofTimestamp,
+      validatorSignature
+    ]);
+
+    return proof;
+}
+
+export default generateProof;
