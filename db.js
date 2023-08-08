7190869b4ec253c40746eb457a0ff01736a417b6
mfonekpo <mfonekpo34@gmail.com>
2023-08-08 10:08:08 +0100

    database setup

--- db.js
+++ db.js
@@ -1,1 +1,23 @@
-export default {}
+// const { PrismaClient } = require('@prisma/client');
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const createUser = async (data) => {
+  return prisma.user.create({ data });
+};
+
+export const createChannel = async (data) => {
+  return prisma.channel.create({ data });
+};
+
+export const createVouching = async (data) => {
+  return prisma.vouching.create({ data });
+};
+
+export const createApiToken = async (data) => {
+  return prisma.api.create({ data });
+};
+
+// Add other CRUD methods as needed
